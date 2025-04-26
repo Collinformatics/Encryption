d08521fce20bfc256422f27ab2221850dca974f4
Collin Andrews <pizzaTrucks@outlook.com>
2025-04-26 14:20:03 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('AES.html') # 'home.html'
+    return render_template('home.html')
 
+@app.route('/encrypt', methods=['POST'])
+def encryptionAES():
+    return render_template('AES.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
