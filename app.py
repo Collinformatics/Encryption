1c8d698d51c6c7021d52a761e0786d4bcf41d57a
Collin Andrews <pizzaTrucks@outlook.com>
2025-04-25 21:40:56 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html',
-        black='#151515', white='#FFFFFF',
-        colorBG='#303030', colorAccent='#454545',
-        colorHeader='#23FF55', buttonHighlight='#1AD747',
-        spacer=20, spacerMini=5,
-        padSide=25, padTB=30, padInput=8,
-        marginB=12, marginButton=12,
-        fontSize=16,
-        borderRad=5,
     )
 
 
