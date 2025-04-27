c36d84800c709597a27a9b66a98d73d72b6ce8a2
Collin Andrews <pizzaTrucks@outlook.com>
2025-04-26 19:27:55 -0500

    Delete static/decrypt.js

--- static/decrypt.js
+++
@@ -1,9 +0,0 @@
-function decrypt(text, password) {
-    console.log(text);
-    if (password === '') {
-        console.log('no password')
-    } else {
-        console.log(password);
-    }
-    return 'text'
-}
